fb208c71c4054a0481da89719d61748170568ac3
Shlok Pandey <shlokpandey1403@gmail.com>
2026-08-09 00:18:56 +0530

    Reverse Integer
    Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
    
    Assume the environment does not allow you to store 64-bit integers (signed or unsigned).

---
+++ 7.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int reverse(int num) {
+        long rev = 0;
+
+        while (num != 0) {
+            int rem = num % 10;
+            rev = rev * 10 + rem;
+            num = num / 10;
+        }
+
+        if (rev > Integer.MAX_VALUE || rev < Integer.MIN_VALUE) {
+            return 0;
+        }
+
+        return (int) rev;
+    }
+}
