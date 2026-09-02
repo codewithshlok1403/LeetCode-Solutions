473fdffc367805f15ec94b939530c7ee38307239
Shlok Pandey <shlokpandey1403@gmail.com>
2026-09-02 10:33:32 +0530

    Left and Right Sum Differences
    You are given a 0-indexed integer array nums of size n.
    
    Define two arrays leftSum and rightSum where:
    
    leftSum[i] is the sum of elements to the left of the index i in the array nums. If there is no such element, leftSum[i] = 0.
    rightSum[i] is the sum of elements to the right of the index i in the array nums. If there is no such element, rightSum[i] = 0.
    Return an integer array answer of size n where answer[i] = |leftSum[i] - rightSum[i]|.
    
     
    
    Example 1:
    
    Input: nums = [10,4,8,3]
    Output: [15,1,11,22]
    Explanation: The array leftSum is [0,10,14,22] and the array rightSum is [15,11,3,0].
    The array answer is [|0 - 15|,|10 - 11|,|14 - 3|,|22 - 0|] = [15,1,11,22].
    Example 2:
    
    Input: nums = [1]
    Output: [0]
    Explanation: The array leftSum is [0] and the array rightSum is [0].
    The array answer is [|0 - 0|] = [0].

---
+++ 2574.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] leftRightDifference(int[] nums) {
+        int n = nums.length;
+        int[] leftSum = new int[n];
+        int[] rightSum = new int[n];
+        int[] ans = new int[n];
+        leftSum[0] = 0;
+        rightSum[n - 1] = 0;
+        for (int i = 1; i < n; i++) {
+            leftSum[i] = nums[i - 1] + leftSum[i - 1];
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            rightSum[i] = nums[i + 1] + rightSum[i + 1];
+        }
+        for (int k = 0; k < n; k++) {
+            ans[k] = rightSum[k] - leftSum[k];
+            if (ans[k] < 0) {
+                ans[k] *= -1;
+            }
+        }
+        return ans;
+    }
+}
